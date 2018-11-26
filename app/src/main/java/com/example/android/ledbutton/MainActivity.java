bb660445e2c0c345c602090b21b11808f186d9cb
Josh Jacobsen <josh.dylan.jacobsen@gmail.com>
2018-11-26 19:34:41 +1300

    Add button control to LED

--- app/src/main/java/com/example/android/ledbutton/MainActivity.java
+++ app/src/main/java/com/example/android/ledbutton/MainActivity.java
@@ -3,14 +3,13 @@ package com.example.android.ledbutton;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.Looper;
 import android.util.Log;
 
 import com.google.android.things.pio.Gpio;
+import com.google.android.things.pio.GpioCallback;
 import com.google.android.things.pio.PeripheralManager;
 
 import java.io.IOException;
-import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends Activity {
 
@@ -18,8 +17,11 @@ public class MainActivity extends Activity {
     private static final String TAG = "MainActivity";
 
     private static final String GREEN_LED_PIN = "BCM19";
+    private static final String BUTTON_PIN = "BCM21";
 
-    private Gpio bus;
+    private Gpio ledBus;
+    private Gpio buttonBus;
+
     private Handler ledToggleHandler;
 
     @Override
@@ -27,67 +29,76 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         if (VERBOSE) Log.v(TAG, "+++ ON CREATE +++");
 
-        PeripheralManager service = PeripheralManager.getInstance();
+        PeripheralManager ledService = PeripheralManager.getInstance();
+        PeripheralManager buttonService = PeripheralManager.getInstance();
 
         try {
-            bus = service.openGpio(GREEN_LED_PIN);
+            ledBus = ledService.openGpio(GREEN_LED_PIN);
+            buttonBus = buttonService.openGpio(BUTTON_PIN);
         } catch (IOException e) {
-            throw new IllegalStateException(GREEN_LED_PIN + " bus cannot be opened.", e);
+            throw new IllegalStateException(GREEN_LED_PIN + " ledBus and/or buttonBus cannot be opened.", e);
         }
 
         try {
-            bus.setDirection(Gpio.DIRECTION_OUT_INITIALLY_HIGH);
-            bus.setActiveType(Gpio.ACTIVE_HIGH);
+            ledBus.setDirection(Gpio.DIRECTION_OUT_INITIALLY_HIGH);
+            ledBus.setActiveType(Gpio.ACTIVE_HIGH);
+
+            buttonBus.setDirection(Gpio.DIRECTION_IN);
+            buttonBus.setActiveType(Gpio.ACTIVE_LOW);
+
         } catch (IOException e) {
-            throw new IllegalStateException(GREEN_LED_PIN + " bus cannot be opened.", e);
+            throw new IllegalStateException(GREEN_LED_PIN + " ledBus and/or buttonBus cannot be opened.", e);
         }
 
-        ledToggleHandler = new Handler(Looper.getMainLooper());
     }
 
     @Override
     protected void onStart(){
         if (VERBOSE) Log.v(TAG, "++ onStart ++" );
         super.onStart();
-        ledToggleHandler.post(toggleLed);
+        try {
+            buttonBus.setEdgeTriggerType(Gpio.EDGE_BOTH);
+            buttonBus.registerGpioCallback(touchButtonACallback);
+        } catch (IOException e) {
+            throw new IllegalStateException(BUTTON_PIN + " cannot be monitored", e);
+        }
+    }
+
+    @Override
+    protected void onStop(){
+        if (VERBOSE) Log.v(TAG, "++ onStop ++" );
+        buttonBus.unregisterGpioCallback(touchButtonACallback);
+        super.onStop();
     }
 
     @Override
     protected void onDestroy(){
         if (VERBOSE) Log.v(TAG, "++ onDestroy ++" );
         try {
-            bus.close();
+            ledBus.close();
+            buttonBus.close();
         } catch (IOException e) {
-            Log.e("MainActivity", GREEN_LED_PIN + " bus cannot be closed. This may cause errors at next launch");
+            Log.e("MainActivity", GREEN_LED_PIN + " ledBus and/or buttonBus cannot be closed. This may cause errors at next launch");
         }
         super.onDestroy();
     }
 
-    private final Runnable toggleLed = new Runnable() {
+    private final GpioCallback touchButtonACallback = new GpioCallback() {
         @Override
-        public void run() {
-            if (VERBOSE) Log.v(TAG, "++ Runnable ++" );
-            boolean isOn;
-
+        public boolean onGpioEdge(Gpio buttonBus) {
             try {
-                isOn = bus.getValue();
-            } catch (IOException e){
-                throw new IllegalStateException(GREEN_LED_PIN + " cannot be read.", e);
-            }
-
-            try {
-                if (isOn) {
-                    if (VERBOSE) Log.v(TAG, "Turning LED off" );
-                    bus.setValue(false);
-                } else {
-                    if (VERBOSE) Log.v(TAG, "Turning LED on" );
-                    bus.setValue(true);
+                if (buttonBus.getValue()) {
+                    Log.v(TAG, "Button pressed; LED on");
+                    ledBus.setValue(true);
                 }
-            } catch (IOException e){
-                throw new IllegalStateException(GREEN_LED_PIN + " cannot be written", e);
+                else {
+                    Log.v(TAG, "Button released; LED off");
+                    ledBus.setValue(false);
+                }
+            } catch (IOException e) {
+                throw new IllegalStateException(BUTTON_PIN + " cannot be read", e);
             }
-
-            ledToggleHandler.postDelayed(this, TimeUnit.SECONDS.toMillis(1));
+            return true;
         }
     };
 }
